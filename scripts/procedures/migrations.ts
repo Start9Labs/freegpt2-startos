bd4627ba9eda885f01f3b60d8c8f15fbada687d7
Mariusz Kogen <k0gen@pm.me>
2024-03-30 15:50:12 +0100

    Updated Ollama to use the latest version 0.1.30

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "2.29.115" );
+    .fromMapping({}, "2.30.115" );
