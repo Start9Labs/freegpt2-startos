f04d5357063e9d5a04220239a1b65a20571fe733
Mariusz Kogen <k0gen@pm.me>
2024-05-11 09:04:19 +0200

    Update ollama backend to 0.1.36

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping({}, "2.34.124" );
+    .fromMapping({}, "2.36.124" );
